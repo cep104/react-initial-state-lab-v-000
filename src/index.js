a9c6a3ba97e32a39eb06e09365d98785d53fbf3d
cep104 <candicelizabethpeters@gmail.com>
2021-01-05 16:24:08 -0600

    commit tree 96d8b61d3dbc12889d2b989c6f656c826c1eef9e

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ImageSlider from './ImageSlider';
-import Bomb from './Bomb';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import ImageSlider from "./ImageSlider";
+import Bomb from "./Bomb";
+// undefined method `[]' for nil:NilClass (NoMethodError) must create components!
 ReactDOM.render(
   <div>
     <ImageSlider />
     <Bomb initialCount={120} />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
